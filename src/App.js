f53d84ebf0f9369bfc2a51b93c556f48df1315a5
yutkts <yutkts@gmail.com>
2020-08-11 22:02:25 +0900

    modified src/App.js

--- src/App.js
+++ src/App.js
@@ -3,10 +3,10 @@ import React, { Component } from 'react';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <h1 className="App-title">Hello World!</h1>
-      </div>
-    )
+      <React.Fragment>
+        <label>hogehoge</label>
+      </React.Fragment>
+    );
   }
 }
 
